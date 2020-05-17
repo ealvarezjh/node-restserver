d66df2faa5edcf338307059f629ebfaade1c079d
Josue Hernandez <ealvarezjh@gmail.com>
2020-05-17 15:25:41 -0600

    Primer commit - POST Y PUT funcionando correctamente

---
+++ README.md
@@ -0,0 +1,14 @@
+# REST Server
+
+## Configuraciones iniciales
+
+* Creación de servidor-web con Express
+    - Configuración de Métodos HTTP (GET, POST, PUT, DELETE).
+    - Configuración de puerto utilizando la variable global >process.
+    - Estructuración del proyecto por carpetas.
+
+
+Es necesario ejecutar:
+```
+    npm install 
+```
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+require('./config/config');
+
+
+// Configuración body-parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+app.use(require('./routes/usuario'));
+
+
+// Conexión: MongoDB(localhost)
+mongoose.connect('mongodb://localhost:27017/cafe', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    (err, res) => {
+        if (err) throw err;
+        console.log(`Connected to MongoDB:OK`);
+    });
+
+
+// Puerto servidorweb
+app.listen(process.env.PORT, () => {
+    console.log(`Listen in port: ${process.env.PORT}`);
+});
